23f0b6701431b35412d9a824f009704cd89be03d
Nichlas Torgersen <fristurt@hotmail.com>
2015-11-19 14:47:18 +0100

    initial commit

---
+++ src/Configuration.java
@@ -0,0 +1,5 @@
+public class Configuration {
+	
+	public String teamName = "obj3000-gr06";
+	
+}
---
+++ src/Droodle.java
@@ -0,0 +1,11 @@
+import storagetool.Storage;
+
+public class Droodle {
+	
+	public static void main(String[] args) {
+		Storage s = new Storage(new Configuration().teamName);
+		
+		
+		
+	}
+}
